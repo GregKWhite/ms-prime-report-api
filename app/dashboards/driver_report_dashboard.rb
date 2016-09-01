8a3671f179004731caac64b762505a6cd08532c9
Greg White <gkwhite95@gmail.com>
2016-08-31 20:35:11 -0400

    Adds administrate assets
    Makes things easier to monkey patch

--- app/dashboards/driver_report_dashboard.rb
+++ app/dashboards/driver_report_dashboard.rb
@@ -27,11 +27,14 @@ class DriverReportDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :created_at,
     :driver,
     :helper,
-    :location,
     :truck,
-    :id,
+    :mileage,
+    :gallons,
+    :amount_paid,
+    :notes,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
